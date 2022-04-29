5f860032c54cf07b0e7b0ef70339181d999e46fb
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-04-29 21:48:32 +0000

    update

---
+++ scripts/0429_lr_5_7e-5.sh
@@ -0,0 +1,15 @@
+# LSBATCH: User input
+#!/bin/bash
+###LSf Syntax
+#BSUB -nnodes 1
+#BSUB -W 720
+#BSUB -G heas
+#BSUB -e cub_gdp_lr5_7e-5.txt
+#BSUB -o cub_gdp_lr5_7e-5.txt
+#BSUB -J cub_gdp_lr5_7e-5
+#BSUB -q pbatch
+cd /usr/workspace/olivare/
+source opence/bin/activate
+cd /g/g20/olivare/alpha-beta-sparsity/
+ 
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -u cub_finetune_gdp.py --imagenet_train_data /usr/workspace/RML-data/data/imagenet/train --imagenet_val_data /usr/workspace/RML-data/data/imagenet/val --data /usr/workspace/RML-data/data/cub/ --rate 0.2 --save_dir /usr/workspace/olivare/cub_gdp --epoch 95 --worker 4 --checkpoint /usr/workspace/olivare/imagenetc_imp_debug3/0checkpoint.pth.tar --lamb 7e-5 --reg-lr 5
