e9f70ce32fb134a5d6d760c8b944d6d03a4b161d
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-05-01 20:26:38 +0000

    update

---
+++ scripts/0501_unroll_lr_5.sh
@@ -0,0 +1,15 @@
+# LSBATCH: User input
+#!/bin/bash
+###LSf Syntax
+#BSUB -nnodes 1
+#BSUB -W 720
+#BSUB -G heas
+#BSUB -e cub_unroll_lr5.txt
+#BSUB -o cub_unroll_lr5.txt
+#BSUB -J cub_unroll_lr5
+#BSUB -q pbatch
+cd /usr/workspace/olivare/
+source opence/bin/activate
+cd /g/g20/olivare/alpha-beta-sparsity/
+ 
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -u cub_finetune_gradient_unroll.py --imagenet_train_data /usr/workspace/RML-data/data/imagenet/imagenet-c.x-full/gaussian_noise/3/ --imagenet_val_data /usr/workspace/RML-data/data/imagenet/val --data /usr/workspace/RML-data/data/cub/ --rate 0.2 --save_dir /usr/workspace/olivare/cub_unroll --epoch 95 --worker 4 --checkpoint /usr/workspace/olivare/imagenetc_imp_debug3/0checkpoint.pth.tar --lamb 1e-4 --reg-lr 5
