cc8670f5a9bcc8da98b8e0e683fcf1f8fba00e01
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-05-18 19:49:54 +0000

    update

--- scripts/0502_cub10_unroll_lr_3.5.sh
+++ scripts/0502_cub10_unroll_lr_3.5.sh
@@ -12,4 +12,4 @@ cd /usr/workspace/olivare/
 source opence/bin/activate
 cd /g/g20/olivare/alpha-beta-sparsity/
  
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -u cub_finetune_gradient_unroll.py --imagenet_train_data /usr/workspace/RML-data/data/imagenet/imagenet-c.x-full/gaussian_noise/3/ --imagenet_val_data /usr/workspace/RML-data/data/imagenet/val --data /usr/workspace/RML-data/data/cub/ --rate 0.2 --save_dir /usr/workspace/olivare/cub10_unroll --epoch 95 --worker 4  --lamb 1e-4 --reg-lr 3.5 --ten-shot --checkpoint /usr/workspace/olivare/imagenetc_imp_debug3/0checkpoint.pth.tar
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -u cub_finetune_gradient_unroll.py --imagenet_train_data /usr/workspace/RML-data/data/imagenet/imagenet-c.x-full/gaussian_noise/3/ --imagenet_val_data /usr/workspace/RML-data/data/imagenet/val --data /usr/workspace/RML-data/data/cub/ --rate 0.2 --save_dir /usr/workspace/olivare/cub10_unroll --epoch 95 --worker 4  --lamb 1e-4 --reg-lr 3.5 --ten-shot --checkpoint /usr/workspace/olivare/imagenetc_imp_debug3/0checkpoint.pth.tar --lower_steps 5
