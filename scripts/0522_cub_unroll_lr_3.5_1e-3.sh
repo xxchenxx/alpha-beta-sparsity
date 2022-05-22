74263292d6ad7a3555cd49d9b9d06bd55904a737
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-05-22 18:39:26 +0000

    update

--- scripts/0522_cub_unroll_lr_3.5_1e-3.sh
+++ scripts/0522_cub_unroll_lr_3.5_1e-3.sh
@@ -4,12 +4,12 @@
 #BSUB -nnodes 1
 #BSUB -W 720
 #BSUB -G heas
-#BSUB -e cub_unroll_lamb_1e-2.txt
-#BSUB -o cub_unroll_lamb_1e-2.txt
-#BSUB -J cub_unroll_lamb_1e-2
+#BSUB -e cub_unroll_lamb_1e-3.txt
+#BSUB -o cub_unroll_lamb_1e-3.txt
+#BSUB -J cub_unroll_lamb_1e-3
 #BSUB -q pbatch
 cd /usr/workspace/olivare/
 source opence/bin/activate
 cd /g/g20/olivare/alpha-beta-sparsity/
  
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -u cub_finetune_gradient_unroll.py --imagenet_train_data /usr/workspace/RML-data/data/imagenet/imagenet-c.x-full/gaussian_noise/3/ --imagenet_val_data /usr/workspace/RML-data/data/imagenet/val --data /usr/workspace/RML-data/data/cub/ --rate 0.2 --save_dir /usr/workspace/olivare/cub_unroll --epoch 95 --worker 4  --lamb 1e-2 --reg-lr 3.5 --checkpoint /usr/workspace/olivare/imagenetc_imp_debug3/0checkpoint.pth.tar --lower_steps 5
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -u cub_finetune_gradient_unroll.py --imagenet_train_data /usr/workspace/RML-data/data/imagenet/imagenet-c.x-full/gaussian_noise/3/ --imagenet_val_data /usr/workspace/RML-data/data/imagenet/val --data /usr/workspace/RML-data/data/cub/ --rate 0.2 --save_dir /usr/workspace/olivare/cub_unroll --epoch 95 --worker 4  --lamb 1e-3 --reg-lr 3.5 --checkpoint /usr/workspace/olivare/imagenetc_imp_debug3/0checkpoint.pth.tar --lower_steps 5
