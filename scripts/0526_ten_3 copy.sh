6df180263f2032ff33163f98e65046c59818c0ae
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-05-27 00:57:25 +0000

    update

--- scripts/0526_ten_3 copy.sh
+++ scripts/0526_ten_3 copy.sh
@@ -12,4 +12,4 @@ cd /usr/workspace/olivare/
 source opence/bin/activate
 cd /g/g20/olivare/alpha-beta-sparsity/
  
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -u cub_finetune_gradient_unroll.py --imagenet_train_data /usr/workspace/RML-data/data/imagenet/train --imagenet_val_data /usr/workspace/RML-data/data/imagenet/val --data /usr/workspace/RML-data/data/cub/ --rate 0.2 --save_dir /usr/workspace/olivare/cub_unroll --epoch 95 --worker 4  --lamb 1e-4 --reg-lr 10 --imagenet-pretrained --lower_steps 5 --seed 3 --ten-shot
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -u cub_finetune_gradient_unroll.py --imagenet_train_data /usr/workspace/RML-data/data/imagenet/train --imagenet_val_data /usr/workspace/RML-data/data/imagenet/val --data /usr/workspace/RML-data/data/cub/ --rate 0.2 --save_dir /usr/workspace/olivare/cub_unroll --epoch 95 --worker 4  --lamb 1e-4 --reg-lr 10 --imagenet-pretrained --lower_steps 5 --ten-shot
